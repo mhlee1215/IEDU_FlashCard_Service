5bbedbc14721931b6eb93aa4bcc34bb1fe74c9e4
mhlee <mhlee1215@gmail.com>
2016-04-10 13:00:26 -0700

    bin added.

---
+++ IEDU_Flashcard_core/src/edu/iedu/flashcard/dao/domain/Word.java
@@ -0,0 +1,21 @@
+package edu.iedu.flashcard.dao.domain;
+
+public class Word {
+	String word;
+	String meaning;
+	
+	public String getWord() {
+		return word;
+	}
+	public void setWord(String word) {
+		this.word = word;
+	}
+	public String getMeaning() {
+		return meaning;
+	}
+	public void setMeaning(String meaning) {
+		this.meaning = meaning;
+	}
+	
+	
+}
---
+++ IEDU_Flashcard_core/src/edu/iedu/flashcard/dao/domain/WordBook.java
@@ -0,0 +1,15 @@
+package edu.iedu.flashcard.dao.domain;
+
+import java.util.List;
+
+public class WordBook {
+	List<Word> wordList;
+
+	public List<Word> getWordList() {
+		return wordList;
+	}
+
+	public void setWordList(List<Word> wordList) {
+		this.wordList = wordList;
+	}
+}
