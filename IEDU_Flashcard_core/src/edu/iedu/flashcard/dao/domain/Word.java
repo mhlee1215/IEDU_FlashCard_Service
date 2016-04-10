2d4dd2df316113eb9892dcf0f51f76a1dbadf391
mhlee <mhlee1215@gmail.com>
2016-04-10 14:53:49 -0700

    wordbook added.

--- IEDU_Flashcard_core/src/edu/iedu/flashcard/dao/domain/Word.java
+++ IEDU_Flashcard_core/src/edu/iedu/flashcard/dao/domain/Word.java
@@ -1,9 +1,21 @@
 package edu.iedu.flashcard.dao.domain;
 
 public class Word {
+	int id;
 	String word;
 	String meaning;
 	
+	public Word(String word, String meaning){
+		this.word = word;
+		this.meaning = meaning;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 	public String getWord() {
 		return word;
 	}
--- IEDU_Flashcard_core/src/edu/iedu/flashcard/dao/domain/WordBook.java
+++ IEDU_Flashcard_core/src/edu/iedu/flashcard/dao/domain/WordBook.java
@@ -3,7 +3,20 @@ package edu.iedu.flashcard.dao.domain;
 import java.util.List;
 
 public class WordBook {
+	String name;
 	List<Word> wordList;
+
+	public WordBook(String name){
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	public List<Word> getWordList() {
 		return wordList;
