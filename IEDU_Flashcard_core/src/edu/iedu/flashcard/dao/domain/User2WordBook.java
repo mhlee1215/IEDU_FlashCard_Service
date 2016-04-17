77e114437b804f4d6714397f1c4187488a3b3778
mhlee <mhlee1215@gmail.com>
2016-04-17 13:27:53 -0700

    class renamed.

---
+++ IEDU_Flashcard_core/src/edu/iedu/flashcard/dao/domain/User2WordBook.java
@@ -0,0 +1,21 @@
+package edu.iedu.flashcard.dao.domain;
+
+public class User2WordBook {
+	int userid;
+	int wordbookid;
+	
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public int getWordbookid() {
+		return wordbookid;
+	}
+	public void setWordbookid(int wordbookid) {
+		this.wordbookid = wordbookid;
+	}
+	
+	
+}
